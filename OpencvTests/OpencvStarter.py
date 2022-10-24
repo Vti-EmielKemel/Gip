15616d53963dece05be47bff72f494fee3b5cac1
Vti-EmielKemel <116551889+Vti-EmielKemel@users.noreply.github.com>
2022-10-24 11:58:00 +0200

    Delete OpencvStarter.py

--- OpencvTests/OpencvStarter.py
+++
@@ -1,8 +0,0 @@
-#openCv import met Genoemt tot cv
-import cv2 as cv
-#leest de pixels binnen van Cat photo (in variabel img)
-img = cv.imread('Resources/Photos/cat.jpg') 
-#maakt een window aan naam 'Picture en displayed img
-cv.imshow('Picture',img)
-#wacht voor een bepaalde tijd tot er een key input is gebeurd 0 = inf.(dan sluit hij het venster)
-cv.waitKey(0)
